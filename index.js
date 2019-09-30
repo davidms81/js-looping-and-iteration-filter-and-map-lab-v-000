b1fc8192205fb8f67c462ac9fb9f0ae9b374ab09
David Schoenholtz <dmschoenholtz@gmail.com>
2019-09-30 16:58:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ function driverNamesWithRevenueOver(drivers, revenue){
   })
 }
 
-function exactMatch (drivers, matcher) {
-  return drivers.filter(function (driver) {
-    let matches = false;
-
-    for (const key in matcher) {
-      matches = driver[key] === matcher[key];
-    }
-
-    return matches;
-  });
+function exactMatch(drivers, attribute){
+  
 }
