6695b3f232ebf50fdbcf4129e786bec5e4a7cea1
David Schoenholtz <dmschoenholtz@gmail.com>
2019-09-30 17:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function driverNamesWithRevenueOver(drivers, revenue){
 function exactMatch(drivers, attribute){
   return drivers.filter(function(driver){
     for (const key in attribute){
-      driver[key] === attribute[key]
+      return driver[key] === attribute[key]
     }
   })
 }
